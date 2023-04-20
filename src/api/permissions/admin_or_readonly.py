63782d253a89ae2553d67961940ced181f91c7a8
Heverton de Paula <h.depaula94@gmail.com>
2023-04-20 18:21:48 -0300

    added jwt authentication

---
+++ src/api/permissions/admin_or_readonly.py
@@ -0,0 +1,5 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class AdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS or request.user.is_staff

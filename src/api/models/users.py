0b3c17d3f606f47d76cfb8dc7e82e5881118b40b
Heverton de Paula <h.depaula94@gmail.com>
2023-04-20 15:42:51 -0300

    removed all fields from serializer and set amount to read_only

--- src/api/models/users.py
+++ src/api/models/users.py
@@ -3,6 +3,10 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 class User(AbstractUser):
+    email=models.EmailField(max_length=254, unique=True)
     amount=models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
     rented_movies=ArrayField(models.CharField(max_length=255), blank=True, default=list)
-    pass
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
--- src/api/serializers/users_serializer.py
+++ src/api/serializers/users_serializer.py
@@ -4,8 +4,10 @@ from django.contrib.auth import get_user_model
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = '__all__'
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['id', 'first_name', 'last_name', 'email', 'password', 'amount']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'amount': {'read_only': True}}
 
     def create(self, validated_data):
         user = get_user_model().objects.create_user(**validated_data)
